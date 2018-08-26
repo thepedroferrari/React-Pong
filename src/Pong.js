0b13904326acb28e388081ddb81525b57f51bfac
Pedro Ferrari <pedro@pedroferrari.com>
2018-08-26 19:34:12 +0200

    Fix coliders and add game controller methods

--- src/Pong.js
+++ src/Pong.js
@@ -19,10 +19,10 @@ class Rectangle {
 		return this.position.x + this.size.x / 2;
 	}
 	get top() {
-		return this.position.y - this.size.x / 2;
+		return this.position.y - this.size.y / 2;
 	}
 	get bottom() {
-		return this.position.y + this.size.x / 2;
+		return this.position.y + this.size.y / 2;
 	}
 }
 
@@ -48,24 +48,57 @@ class Pong extends Component {
 
 		// Set the Ball
 		this.ball = new Ball();
-		this.ball.velocity.x = -200;
-		this.ball.velocity.y = 200;
-		this.ball.position.x = this.canvas.width / 2;
-		this.ball.position.y = this.canvas.height / 2;
 
 		// Set the Players
 		this.players = [ new Player(), new Player() ];
-		this.players[0].position.x = 40;
-		this.players[1].position.x = this.canvas.width - 40;
+		this.players[0].position.x = 100;
+		this.players[1].position.x = this.canvas.width - 100;
 		this.players.forEach((player) => (player.position.y = this.canvas.height / 2 - player.size.y / 2));
 
 		// Setup the animation
 		this.lastTime = null;
 		this.callback(1000);
-		console.log(this.players);
+		this.reset();
+		this.flag = false;
 	}
 
 	componentDidUpdate() {}
+
+	flagger = () => {
+		// Function meant for testing
+		this.flag = !this.flag;
+
+		console.log(
+			'1PLA:',
+			this.players[0]
+			// '1PLA Y:',
+			// Math.floor(this.players[0].position.y)
+		);
+		console.log(
+			'2PLA:',
+			this.players[1]
+			// '2PLA Y:',
+			// Math.floor(this.players[1].position.y)
+		);
+		console.log('Ball', this.ball);
+	};
+
+	checkCollided(val1, val2) {
+		if (val1 - val2 < 20) {
+			return true;
+		}
+	}
+
+	collide = (player, ball) => {
+		if (
+			player.left < ball.right &&
+			player.right > ball.left &&
+			player.top < ball.bottom &&
+			player.bottom > ball.top
+		) {
+			this.ball.velocity.x = -this.ball.velocity.x;
+		}
+	};
 
 	draw() {
 		// Paint Context
@@ -78,9 +111,17 @@ class Pong extends Component {
 		// Paint the Players
 		this.players.forEach((player) => this.drawRectangle(player));
 	}
+
 	drawRectangle(rectangle) {
 		this.context.fillStyle = 'white';
 		this.context.fillRect(rectangle.left, rectangle.top, rectangle.size.x, rectangle.size.y);
+	}
+
+	reset() {
+		this.ball.velocity.x = 800;
+		this.ball.velocity.y = 100;
+		this.ball.position.x = 0;
+		this.ball.position.y = 0;
 	}
 
 	update(deltaTime) {
@@ -89,14 +130,20 @@ class Pong extends Component {
 
 		// Prevent going outside canvas
 		if (this.ball.left < 0 || this.ball.right > this.canvas.width) {
-			this.ball.velocity.x = -this.ball.velocity.x; // invert direction
+			const playerId = (this.ball.velocity.x < 0) | 0;
+			this.players[playerId].score++;
+			this.reset();
 		}
 
 		if (this.ball.top < 0 || this.ball.bottom > this.canvas.height) {
 			this.ball.velocity.y = -this.ball.velocity.y; // invert direction
 		}
 
+		// Impossible to beat AI that always follow the ball
 		this.players[1].position.y = this.ball.position.y;
+
+		// Adding Collision to each Player
+		this.players.forEach((player) => this.collide(player, this.ball));
 
 		// Paint
 		this.draw();
@@ -120,7 +167,14 @@ class Pong extends Component {
 		return (
 			<React.Fragment>
 				<h1>Pong</h1>
-				<canvas id="pong" ref="pong" width={1200} height={800} onMouseMove={this.handleMouseMove} />
+				<canvas
+					id="pong"
+					ref="pong"
+					width={1200}
+					height={800}
+					onMouseMove={this.handleMouseMove}
+					onClick={this.flagger}
+				/>
 			</React.Fragment>
 		);
 	}
